3e6b7d490a648a9397a0cc6b72f50ae69adacbf5
Taimoor Ali <taimoorali083@gmail.com>
2023-04-14 11:46:32 +0500

    CRUD using reduxtoolkit(RTK)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react'
 // import { useGetAllPostsQuery } from './services/post'
 // import {useGetPostByIdQuery} from './services/post'
- import {useGetPostByLimitQuery} from './services/post'
-function App() {
+//  import {useGetPostByLimitQuery} from './services/post'
+//  import {useDeletePostMutation} from './services/post'
+//  import {useCreatePostMutation} from './services/post'
+ import {useUpdatePostMutation} from './services/post'
+function App() { 
   //eslint-disable-next-line
   // const responseInfo = useGetAllPostsQuery();
   // const   responseInfo  = useGetPostByIdQuery(22);
-  const   responseInfo  = useGetPostByLimitQuery(3);
-  
-    // console.log("Response Information  :",responseInfo)
-    console.log("Data Taimoor  :",responseInfo.data)
-  //   console.log("Success Taimoor  :",responseInfo.isSuccess)
-  //   console.log("Fetching Taimoor  :",responseInfo.isFetching)
-  //   console.log("isLoading Taimoor  :",responseInfo.isLoading)
-  //   console.log("isUninitialized Taimoor  :",responseInfo.isUninitialized)
+  // const   responseInfo  = useGetPostByLimitQuery(3);
+  //  const [ deletePost , responseInfo ] = useDeletePostMutation()
+  //  const [ createPost , responseInfo ] = useCreatePostMutation()
+  // const newPost ={
+  //   title: "New Title",
+  //   body: "New body added",
+  //   userId:1,
+  // }
+
+
+
+   const [ updatePost , responseInfo ] = useUpdatePostMutation()
+
+const updatePostData ={
+  id:1,
+  title: "New Title update",
+  body: "New body update",
+  userId:1,
+}
+    console.log("Response Information  :",updatePost)
+    console.log("Data Taimoor  :",responseInfo)
+    // console.log("Success Taimoor  :",responseInfo.isSuccess)
+    console.log("Success Taimoor  :",responseInfo.data)
+    
+    // console.log("Fetching Taimoor  :",responseInfo.isFetching)
+    // console.log("isLoading Taimoor  :",responseInfo.isLoading)
+    // console.log("isUninitialized Taimoor  :",responseInfo.isUninitialized)
   //   console.log("isError Taimoor  :",responseInfo.isError)
   // if(responseInfo.isError) return console.log(responseInfo.error.error)
 
@@ -42,17 +64,43 @@ function App() {
   {/* //Limited number of data shows */}
 
 
-<div className="container">
+{/* <div className="container">
 {responseInfo.data && responseInfo.data.map((post , i)=>
         <div key={i}><h1>{post.id}. {post.title}</h1>
         <p>{post.body}</p>
         <hr></hr></div>) } 
+</div> */}
+
+
+{/* Deletes Data */}
+{/* <div className='App'>
+<h1>Redux ToolKit  -  RTK Query (Delete Data)</h1>
+
+ {/* {console.log("okna",deletePost) } */}
+ {/* <div> <button onClick={()=>deletePost(2)}>Delete Post</button> </div> */}
+{/* </div>  */}
+
+
+
+{/* // Creates data(Posts) */}
+
+{/* <div className='App'>
+<h1>Redux ToolKit  -  RTK Query (Create Data)</h1>
+
+ {/* {console.log("okna",deletePost) } */}
+ {/* <div> <button onClick={()=>createPost(newPost)}>Add Post</button> </div>
+</div>  */}
+
+
+{/* // update data(Posts) */}
+
+
+<div className='App'>
+<h1>Redux ToolKit  -  RTK Query (update Data)</h1>
+
+ {/* {console.log("okna",deletePost) } */}
+ <div> <button onClick={()=>updatePost(updatePostData)}>UPDATE Post</button> </div>
 </div>
-
-
-
-
-
 
       </React.Fragment>)
 }
--- src/services/post.js
+++ src/services/post.js
@@ -1,41 +1,85 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
- //post are like fb-posts etc & GET api req to fetch all data(posts)
+//post are like fb-posts etc & GET api req to fetch all data(posts)
 
-export const postApi = createApi({                                      
-    reducerPath: 'postApi',
-    baseQuery: fetchBaseQuery({ 
-        baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+export const postApi = createApi({
+  reducerPath: 'postApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://jsonplaceholder.typicode.com/'
+  }),
 
-    endpoints: (builder) => ({                //getting all data
-      getAllPosts: builder.query({            // use then getAllPosts then query for exporting hook =useGetAllPostsQuery
-        query: () => ({
-            url:'posts',                     //it is endpoint  e.g,https://jsonplaceholder.typicode.com/posts
-            method:'GET'
-        }),
+  endpoints: (builder) => ({                //getting all data
+    getAllPosts: builder.query({            // use then getAllPosts then query for exporting hook =useGetAllPostsQuery
+      query: () => ({
+        url: 'posts',                     //it is endpoint  e.g,https://jsonplaceholder.typicode.com/posts
+        method: 'GET'
       }),
-      getPostById: builder.query({
-        query:(id)=>{
-        console.log("ID : ", id)
-          return{
-          url:`posts/${id}`,
-          method:'GET'
-        }}
-       }),
-       getPostByLimit: builder.query({
-        query:(num)=>{
-        console.log("Limit Number : ", num)
-          return{
-          url:`posts/?_limit=${num}`,
-          method:'GET'
-        }}
-       }),
-       
     }),
-    
-    
-  })
-  
-  // Export hooks for usage in functional components, which are
-  // auto-generated based on the defined endpoints
-  export const { useGetAllPostsQuery , useGetPostByIdQuery , useGetPostByLimitQuery} = postApi
+    getPostById: builder.query({
+      query: (id) => {
+        console.log("ID : ", id)
+        return {
+          url: `posts/${id}`,
+          method: 'GET'
+        }
+      }
+    }),
+    getPostByLimit: builder.query({
+      query: (num) => {
+        console.log("Limit Number : ", num)
+        return {
+          url: `posts/?_limit=${num}`,
+          method: 'GET'
+        }
+      }
+    }),
+    createPost: builder.mutation({
+      query: (newPost) => {
+        console.log("Post Create: ", newPost)
+        return {
+          url: 'posts',
+          method: 'POST',
+          body: newPost,
+          headers: {
+            'Content-type': 'application/json, charset-UTF-8'
+          }
+        }
+      }
+    }),
+    deletePost: builder.mutation({
+      query: (id) => {
+        console.log("Delete Id: ", id)
+        return {
+          url: `posts/${id}`,
+          method: 'DELETE'
+        }
+      }
+    }),
+    updatePost: builder.mutation({
+      query: (updatePostData) => {
+        console.log("Post update: ", updatePostData);
+
+        const { id , ...data }= updatePostData; 
+
+        console.log("Post update actual: ", data)
+
+        return {
+          url:`posts/${id}`,
+          method: 'PUT',
+          body: data,
+          headers: {
+            'Content-type': 'application/json, charset-UTF-8'
+          }
+        }
+      }
+    }),
+
+
+  }),
+
+
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetAllPostsQuery, useGetPostByIdQuery, useGetPostByLimitQuery, useDeletePostMutation, useCreatePostMutation , useUpdatePostMutation } = postApi
